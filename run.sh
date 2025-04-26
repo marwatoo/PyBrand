fdf420f3c25cd1be6567880f747dacb212aa3f3a
marwatoo <bifisse.marwa@gmail.com>
2025-04-26 13:23:44 +0100

    updated

--- run.sh
+++ run.sh
@@ -4,16 +4,16 @@
 if command -v wmctrl &> /dev/null; then
     WM_NAME=$(wmctrl -m | grep "Name" | awk '{print $2}')
     if [ "$WM_NAME" == "KWin" ]; then
-        python3 "/home/marwa/Documents/apps/PyBrand/main.py"
+        python3 "./main.py"
     else
-        python3 "/home/marwa/Documents/apps/PyBrand/pyside.py"
+        python3 "./pyside.py"
     fi
 elif command -v xprop &> /dev/null; then
     WM_NAME=$(xprop -root _NET_WM_NAME | cut -d ' ' -f 3-)
     if [ "$WM_NAME" == "KWin" ]; then
-        python3 "/home/marwa/Documents/apps/PyBrand/main.py"
+        python3 "./main.py"
     else
-        python3 "/home/marwa/Documents/apps/PyBrand/pyside.py"
+        python3 "./pyside.py"
     fi
 else
     echo "Error somewhere. Sorry.."
